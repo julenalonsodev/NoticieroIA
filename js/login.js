b5109d21b12a2f8a110ced6408e71a9687a420d0
julenalonsodev <julen.alonso.dev@gmail.com>
2025-11-17 13:18:43 +0100

    Web

---
+++ js/login.js
@@ -0,0 +1,5 @@
+        // Simulación de login (sin conexión a base de datos)
+        document.getElementById('loginForm').addEventListener('submit', function (e) {
+            e.preventDefault();
+            window.location.href = 'home.html'; // Redirección ficticia
+        });
